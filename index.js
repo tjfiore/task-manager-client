54af7f82e06f09acdf40fdc9a446b3bcd7582838
damian.wynter <damian.wynter@particularpresence.com>
2017-08-08 15:30:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:auth/auth');
+
+
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port);
+console.log('server listening on port: ', port);
